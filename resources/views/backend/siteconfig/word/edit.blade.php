54054175216f5014dc4a86fb00f33b6067e7faac
IcicleCorporation <Dev@iciclecorporation.com>
2023-01-22 16:23:26 +0600

    bedCabin & bedWord

---
+++ resources/views/backend/siteconfig/word/edit.blade.php
@@ -0,0 +1,29 @@
+<div class="modal-content">
+    <form class="needs-validation" id="bedWord_edit_form" action="{{ route('backend.siteconfig.bedWord.update', $bedWord) }}" method="Post"
+        enctype="multipart/form-data">
+        @method('POST')
+        @csrf
+        <div class="modal-header">
+            <h4 class="title" id="">Floor Edit</h4>
+        </div>
+        <div class="modal-body">
+            <div class="form-validation">
+                <div class="form-group">
+                    @include('components.backend.forms.input.input-type',[ 'name' => 'name', 'value' => $bedWord->name, 'placeholder' => 'name will be here...', 'required'=>true ])
+                    @include('components.backend.forms.input.errorMessage', ['message' => $errors->first('name')])
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" name="status" {{$bedWord->status == true ? 'Checked': ' '}} id="active_check">
+                    <label class="form-check-label" for="active_check">Active ?</label>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-primary edit_bedWord_button">SAVE</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">CLOSE</button>
+        </div>
+    </form>
+</div>
+
