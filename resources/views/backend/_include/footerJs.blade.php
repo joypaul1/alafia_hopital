67ac5f05a9ecae2156c9e7e78ebb9be1fb542558
joypaul1 <joypaul.bd1997@gmail.com>
2023-03-15 15:54:32 +0600

    dashboard

--- resources/views/backend/_include/footerJs.blade.php
+++ resources/views/backend/_include/footerJs.blade.php
@@ -1,11 +1,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ {{-- <script src="{{ asset('assets/backend') }}/bundles/libscripts.bundle.js"></script> --}}
  <script src="{{ asset('assets/backend') }}/bundles/libscripts.bundle.js"></script>
  <script src="{{ asset('assets/backend') }}/bundles/vendorscripts.bundle.js"></script>
- {{-- <script src="{{ asset('assets/backend') }}/bundles/jvectormap.bundle.js"></script> --}}
- {{-- <script src="{{ asset('assets/backend') }}/bundles/morrisscripts.bundle.js"></script> --}}
- {{-- <script src="{{ asset('assets/backend') }}/bundles/knob.bundle.js"></script> --}}
+ <script src="{{ asset('assets/backend') }}/bundles/jvectormap.bundle.js"></script>
+ <script src="{{ asset('assets/backend') }}/bundles/morrisscripts.bundle.js"></script>
+ <script src="{{ asset('assets/backend') }}/bundles/knob.bundle.js"></script>
  <script src="{{ asset('assets/backend') }}/bundles/mainscripts.bundle.js"></script>
- <script src="{{ asset('assets/backend') }}/js/index8.js"></script>
+ <script src="{{ asset('assets/backend') }}/js/index4.js"></script>
  <script src="{{ asset('assets/backend') }}/vendor/toastr/toastr.js"></script>
 <!-- Footer end -->
 
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -70,6 +70,159 @@
 
 @section('content')
     <div class="row clearfix">
+        <div class="col-lg-3 col-md-6">
+            <div class="card top_counter">
+                <div class="body">
+                    <div class="icon text-info"><i class="fa fa-user"></i> </div>
+                    <div class="content">
+                        <div class="text">Total Student</div>
+                        <h5 class="number">530</h5>
+                    </div>
+                    <hr>
+                    <div class="icon text-warning"><i class="fa fa-user-circle"></i> </div>
+                    <div class="content">
+                        <div class="text">Total Teacher</div>
+                        <h5 class="number">14</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card top_counter">
+                <div class="body">
+                    <div class="icon text-warning"><i class="fa fa-tags"></i> </div>
+                    <div class="content">
+                        <div class="text">Department</div>
+                        <h5 class="number">7</h5>
+                    </div>
+                    <hr>
+                    <div class="icon"><i class="fa fa-graduation-cap"></i> </div>
+                    <div class="content">
+                        <div class="text">Courses</div>
+                        <h5 class="number">35</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card top_counter">
+                <div class="body">
+                    <div class="icon text-danger"><i class="fa fa-credit-card"></i> </div>
+                    <div class="content">
+                        <div class="text">Expense</div>
+                        <h5 class="number">$3205</h5>
+                    </div>
+                    <hr>
+                    <div class="icon text-success"><i class="fa fa-university"></i> </div>
+                    <div class="content">
+                        <div class="text">Income</div>
+                        <h5 class="number">$35,325</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card top_counter">
+                <div class="body">
+                    <div class="icon"><i class="fa fa-map-pin"></i> </div>
+                    <div class="content">
+                        <div class="text">Our Center</div>
+                        <h5 class="number">28</h5>
+                    </div>
+                    <hr>
+                    <div class="icon text-success"><i class="fa fa-smile-o"></i> </div>
+                    <div class="content">
+                        <div class="text">Happy Clients</div>
+                        <h5 class="number">528</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header text-center text-white"
+                    style="
+                    background: #ff5925;
+                    font-weight: bold;
+                    font-size: 14px;
+                ">
+                    Weekly Sell Report</div>
+                <canvas id="weeklySell"></canvas>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header text-center text-white"
+                    style="
+                    background: #8d4df6;
+                    font-weight: bold;
+                    font-size: 14px;
+                ">
+                    Monthly Sell Report</div>
+                <canvas id="monthlySell"></canvas>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header text-center text-white"
+                    style="
+                    background: #8d4df6;
+                    font-weight: bold;
+                    font-size: 14px;
+                ">
+                    Today's Sellman Wise Sell Report</div>
+                <canvas id="userSell"></canvas>
+            </div>
+        </div>
+        {{-- <div class="col-lg-6 col-md-6">
+            @include('backend._partials.todo')
+        </div> --}}
+    </div>
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="header">
+                    <h2>University Survey</h2>
+                    <ul class="header-dropdown">
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                aria-haspopup="true" aria-expanded="false"></a>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a href="javascript:void(0);">Action</a></li>
+                                <li><a href="javascript:void(0);">Another Action</a></li>
+                                <li><a href="javascript:void(0);">Something else</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="body">
+                    <div class="row text-center">
+                        <div class="col-sm-3 col-6">
+                            <h4 class="margin-0">$231</h4>
+                            <p class="text-muted margin-0"> Today's</p>
+                        </div>
+                        <div class="col-sm-3 col-6">
+                            <h4 class="margin-0">$1,254</h4>
+                            <p class="text-muted margin-0">This Week's</p>
+                        </div>
+                        <div class="col-sm-3 col-6">
+                            <h4 class="margin-0">$3,298</h4>
+                            <p class="text-muted margin-0">This Month's</p>
+                        </div>
+                        <div class="col-sm-3 col-6">
+                            <h4 class="margin-0">$9,208</h4>
+                            <p class="text-muted margin-0">This Year's</p>
+                        </div>
+                    </div>
+                    <div id="m_bar_chart" class="graph m-t-20"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row clearfix">
         <div class="col-12">
             <div class="card">
                 <div class="body">
@@ -92,15 +245,25 @@
     <script>
         var userSellData = [];
         var userSellAmount = [];
+        var monthlySellData = [];
+        var monthlySellAmount = [];
         $(document).ready(function() {
+            // weaklyData
             let weaklyData = jQuery.parseJSON('{!! json_encode($userSell) !!}');
-            console.log(weaklyData);
             weaklyData.forEach(element => {
                 userSellData.push(element.name);
                 userSellAmount.push(Math.round(element.total_sell));
             });
-            // console.log(userSellData,userSellAmount);
 
+            // monthlyData
+            let monthlyData = jQuery.parseJSON('{!! json_encode($monthData) !!}');
+            $.each(monthlyData, function(key, value) {
+                monthlySellData.push(key);
+                monthlySellAmount.push(Math.round(value));
+
+            });
+
+            // userSell
             const userSell = document.getElementById('userSell');
             new Chart(userSell, {
                 type: 'bar',
@@ -135,40 +298,76 @@
                 }
             });
 
-        });
-        const weeklySell = document.getElementById('weeklySell');
-        new Chart(weeklySell, {
-            type: 'line',
-            data: {
-
-                labels: jQuery.parseJSON('{!! json_encode($weaklyData['days']) !!}'),
-                datasets: [{
-                    label: '# of Votes',
-                    data: jQuery.parseJSON('{!! json_encode($weaklyData['sell']) !!}'),
-                    borderWidth: 2
-                }]
-            },
-            options: {
-                animations: {
-                    tension: {
-                        duration: 1000,
-                        easing: 'linear',
-                        from: 1,
-                        to: 0,
-                        loop: true
-                    }
+            // monthlySell
+            const monthlySell = document.getElementById('monthlySell');
+            new Chart(monthlySell, {
+                type: 'bar',
+                data: {
+                    labels: monthlySellData,
+                    datasets: [{
+                        data: monthlySellAmount,
+                        borderWidth: 2,
+                        backgroundColor: 'green',
+                    }]
                 },
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false
+                options: {
+                    animations: {
+                        tension: {
+                            duration: 1000,
+                            easing: 'linear',
+                            from: 1,
+                            to: 0,
+                            loop: true
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
                     }
                 }
-            }
+            });
+            //weeklysell
+            const weeklySell = document.getElementById('weeklySell');
+            new Chart(weeklySell, {
+                type: 'line',
+                data: {
+
+                    labels: jQuery.parseJSON('{!! json_encode($weaklyData['days']) !!}'),
+                    datasets: [{
+                        label: '# of Votes',
+                        data: jQuery.parseJSON('{!! json_encode($weaklyData['sell']) !!}'),
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    animations: {
+                        tension: {
+                            duration: 1000,
+                            easing: 'linear',
+                            from: 1,
+                            to: 0,
+                            loop: true
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+
         });
     </script>
 @endpush
