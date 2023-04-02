e5cb55a96aacc8a83fcdbdb19bffa0370579913b
joypaul1 <joypaul.bd1997@gmail.com>
2023-04-02 11:03:59 +0600

    service invoice modal

--- app/Models/Service/ServiceInvoice.php
+++ app/Models/Service/ServiceInvoice.php
@@ -2,10 +2,53 @@
 
 namespace App\Models\Service;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\DailyAccountTransaction;
+use App\Models\Patient\Patient;
+use App\Models\Transaction\CashFlow;
+use App\Traits\AutoTimeStamp;
+use App\Traits\GlobalScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServiceInvoice extends Model
 {
-    use HasFactory;
+    use GlobalScope, AutoTimeStamp;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Get all of the Purchase's daybook transaction.
+     */
+    public function dailyTransactions()
+    {
+        return $this->morphMany(DailyAccountTransaction::class, 'transactionable');
+    }
+    /**
+     * Get all of the Purchase's cash flow transaction.
+     */
+    public function cashflowTransactions()
+    {
+        return $this->morphMany(CashFlow::class, 'cashflowable');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class, 'patient_id', 'id');
+    }
+
+    public function paymentHistories()
+    {
+        return $this->hasMany(ServiceInvoicePaymentHistory::class, 'service_invoice_id', 'id');
+    }
+
+    /**
+     * Get all of the testTube for the LabInvoice
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function labTestDetails(): HasMany
+    {
+        return $this->hasMany(ServiceInvoiceItems::class, 'service_invoice_id', 'id');
+    }
+
 }
--- app/Models/Service/ServiceInvoicePaymentHistory.php
+++ app/Models/Service/ServiceInvoicePaymentHistory.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Service;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\AutoTimeStamp;
+use App\Traits\GlobalScope;
 
 class ServiceInvoicePaymentHistory extends Model
 {
-    use HasFactory;
+    use GlobalScope, AutoTimeStamp;
+    protected $guarded = ['id'];
 }
