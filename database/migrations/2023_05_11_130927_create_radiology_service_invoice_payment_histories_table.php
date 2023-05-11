d8932ea141fa0b18ddd5ab27d05ee6b0aa1d6ecb
joypaul1 <joypaul.bd1997@gmail.com>
2023-05-11 13:26:54 +0600

    DB added

--- database/migrations/2023_05_11_130927_create_radiology_service_invoice_payment_histories_table.php
+++ database/migrations/2023_05_11_130927_create_radiology_service_invoice_payment_histories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,7 +15,7 @@ return new class extends Migration
     public function up()
     {
 
-        // ALTER TABLE `radiology_service_invoice_payment_histories` ADD FOREIGN KEY (`service_invoice_id`) REFERENCES `radiology_service_invoices`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
         Schema::create('radiology_service_invoice_payment_histories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('service_invoice_id');
@@ -25,6 +26,7 @@ return new class extends Migration
             $table->string('note')->nullable();
             $table->timestamps();
         });
+        DB::Statement('ALTER TABLE `radiology_service_invoice_payment_histories` ADD FOREIGN KEY (`service_invoice_id`) REFERENCES `radiology_service_invoices`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
     }
 
     /**
