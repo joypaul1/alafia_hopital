fe064fc12a3988f97a407879ea92c4c83bd2819a
joypaul1 <joypaul.bd1997@gmail.com>
2023-05-29 11:24:21 +0600

    reportDesign

--- routes/web.php
+++ routes/web.php
@@ -20,36 +20,21 @@ Route::get('print', function () {
 });
 Route::get('/',[LoginController::class, 'showLoginForm'])->name('home');
 Route::get('/reportDesign', function () {
-//    return  $labTest =LabTest::whereNull('short_name')->get();
-    // foreach ($labTest as $key => $value) {
-    //     $value->update([
-    //         'needle' => '1',
-    //     ]);
-    // }
-    // $labTest =LabTest::where('category', 'Urine')->with('tube')->get();
-    // foreach ($labTest as $key => $value) {
-    //     $value->update([
-    //         'needle' => '0',
-    //         'pot' => '1',
-    //         'lab_test_tube_id' => '6',
-    //     ]);
-    // }
-    // dd('done',$labTest);
-    // $units = (object)[
-    //     ['id' => 'mg/dl', 'name' => 'mg/dl'],
-    //     ['id' => 'mmol/l', 'name' => 'mmol/l'],
-    //     ['id' => 'Nil', 'name' => 'Nil'],
-    //     ['id' => 'µg/dl' , 'name' => 'µg/dl' ],
-    //     ['id' => 'U/L' , 'name' => 'U/L' ],
-    //     ['id' => 'g/dl' , 'name' => 'g/dl' ],
-    //     ['id' => 'mmol/l' , 'name' => 'mmol/l' ],
-    //     ['id' => '%' , 'name' => '%' ],
-    //     ['id' => 'N/A' , 'name' => 'N/A' ],
-    //     ['id' => 'IU/mL' , 'name' => 'IU/mL' ],
-    //     ['id' => 'mg/L' , 'name' => 'mg/L' ],
-    // ];
-    // return view('backend.dashboard.lab-report', compact('units'));
-    // return view('backend.dashboard.lab-report(CultureofBlood)', compact('units'));
+
+    $units = (object)[
+        ['id' => 'mg/dl', 'name' => 'mg/dl'],
+        ['id' => 'mmol/l', 'name' => 'mmol/l'],
+        ['id' => 'Nil', 'name' => 'Nil'],
+        ['id' => 'µg/dl' , 'name' => 'µg/dl' ],
+        ['id' => 'U/L' , 'name' => 'U/L' ],
+        ['id' => 'g/dl' , 'name' => 'g/dl' ],
+        ['id' => 'mmol/l' , 'name' => 'mmol/l' ],
+        ['id' => '%' , 'name' => '%' ],
+        ['id' => 'N/A' , 'name' => 'N/A' ],
+        ['id' => 'IU/mL' , 'name' => 'IU/mL' ],
+        ['id' => 'mg/L' , 'name' => 'mg/L' ],
+    ];
+    return view('backend.dashboard.lab-report(CultureofBlood)', compact('units'));
     // return view('backend.dashboard.lab-report(Serology)', compact('units'));
     // return view('backend.dashboard.lab-report(CultureofBlood)', compact('units'));
 });
