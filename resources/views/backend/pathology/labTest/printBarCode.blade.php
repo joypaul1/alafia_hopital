5bd86eed8cd2e0fece5933b8f150c5728a98db74
joypaul1 <joypaul.bd1997@gmail.com>
2023-06-01 15:32:31 +0600

    str_replace

--- resources/views/backend/pathology/labTest/printBarCode.blade.php
+++ resources/views/backend/pathology/labTest/printBarCode.blade.php
@@ -131,8 +131,6 @@
                                 </p>
                                 <p>
                                     @php
-                                        // $dep = str_replace('(', ' ', $key);
-                                        // $dep = str_replace(')', ' ', $key);
                                         $dep = str_replace(array( '(', ')' ), '', $dep);
                                         $dep = preg_replace("/[0-9]+/", '', $dep);
                                     @endphp
